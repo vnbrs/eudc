303646034a20f463f22866c474efee9faa2cd750
Vinicius Brasil <marcosbrasilaraujo@gmail.com>
2018-10-12 12:45:52 -0300

    Add additional information to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,13 @@ from setuptools import setup
 
 setup(
     name='udce',
+    description='''
+        A CLI tool that helps you to find and exterminatate unnecessary debugging calls on your code such as `console.log`, `byebug` and so on.
+    ''',
+    url='https://github.com/vnbrs/udce',
     version='1.0',
+    author='Vinicius Brasil',
+    author_email='vnbrs@icloud.com',
     py_modules='udce',
     install_requires=['Click',],
     entry_points='''
