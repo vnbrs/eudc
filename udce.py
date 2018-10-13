fa5eefb1396a33f9de123571a77d864e90c3bc57
Gyrotron <simmarkalsi@gmail.com>
2018-10-14 04:27:31 +0530

    Removed Whitespace

--- udce.py
+++ udce.py
@@ -30,6 +30,6 @@ def check_file(file, language):
             for check_expression in language.debugging_calls:
                 if re.match(check_expression, line):
                     click.echo(f'\nFound debugging call on ', nl=False)
-                    click.secho(f'{file}: ', fg='red', nl=False)
+                    click.secho(f'{file}:', fg='red', nl=False)
                     click.secho(f'{line_number}', fg='green')
                     click.echo(line)
