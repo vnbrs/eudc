0b1326994802021ebf64e836cdc8f0b8df29efbc
Gyrotron <simmarkalsi@gmail.com>
2018-10-14 03:42:29 +0530

    CLI output is now colored

--- udce.py
+++ udce.py
@@ -29,5 +29,7 @@ def check_file(file, language):
             line_number += 1
             for check_expression in language.debugging_calls:
                 if re.match(check_expression, line):
-                    click.echo(f'\nFound debugging call on {file}:{line_number}')
+                    click.echo(f'\nFound debugging call on ', nl=False)
+                    click.secho(f'{file}: ', fg='red', nl=False)
+                    click.secho(f'{line_number}', fg='green')
                     click.echo(line)
