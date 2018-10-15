8fd9c29203d90d8772d4a48bdd409d34678fd1f8
Renan Traba <hellupline@gmail.com>
2018-10-15 15:24:03 -0300

    correct import way

---
+++ language/__init__.py
@@ -0,0 +1,4 @@
+from .GoLanguage import GoLanguage  # noqa
+from .JavaScriptLanguage import JavaScriptLanguage  # noqa
+from .PythonLanguage import PythonLanguage  # noqa
+from .RubyLanguage import RubyLanguage  # noqa
--- udce.py
+++ udce.py
@@ -1,26 +1,31 @@
-import click, glob, os, re
-from language.RubyLanguage import RubyLanguage
-from language.PythonLanguage import PythonLanguage
-from language.JavaScriptLanguage import JavaScriptLanguage
-from language.GoLanguage import GoLanguage
+import glob
+import os
+import re
+
+import click
+
+from language import GoLanguage, JavaScriptLanguage, PythonLanguage, RubyLanguage
 
 FILE_TYPES = {
-    'ruby': RubyLanguage,
-    'python': PythonLanguage,
-    'javascript': JavaScriptLanguage,
-    'go': GoLanguage
+    "ruby": RubyLanguage,
+    "python": PythonLanguage,
+    "javascript": JavaScriptLanguage,
+    "go": GoLanguage,
 }
 
+
 @click.command()
-@click.argument('search_directory',
-                type=click.Path(resolve_path=True))
-@click.option('-l', '--languages',
-              prompt='Programming languages',
-              help=f'The programming languages to search for debugging calls.')
+@click.argument("search_directory", type=click.Path(resolve_path=True))
+@click.option(
+    "-l",
+    "--languages",
+    prompt="Programming languages",
+    help=f"The programming languages to search for debugging calls.",
+)
 def cli(search_directory, languages):
-    for language in languages.split(','):
+    for language in languages.split(","):
         os.chdir(search_directory)
-        for file in glob.glob(f'**/*.{FILE_TYPES[language].extension}', recursive=True):
+        for file in glob.glob(f"**/*.{FILE_TYPES[language].extension}", recursive=True):
             check_file(file, FILE_TYPES[language])
 
 
@@ -31,7 +36,7 @@ def check_file(file, language):
             line_number += 1
             for check_expression in language.debugging_calls:
                 if re.match(check_expression, line):
-                    click.echo(f'\nFound debugging call on ', nl=False)
-                    click.secho(f'{file}:', fg='red', nl=False)
-                    click.secho(f'{line_number}', fg='green')
+                    click.echo(f"\nFound debugging call on ", nl=False)
+                    click.secho(f"{file}:", fg="red", nl=False)
+                    click.secho(f"{line_number}", fg="green")
                     click.echo(line)
